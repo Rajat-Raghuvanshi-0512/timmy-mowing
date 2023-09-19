254d3bbfdc00367574a8b0fb17f57b33267b50b5
Rajat <rajat.karnal@gmail.com>
2023-09-19 21:29:59 +0530

    Minor changes in modal

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -13,12 +13,12 @@ const Modal = ({
     <>
       <div
         className={` fixed w-screen top-0 left-0 h-screen z-50 select-none bg-black bg-opacity-40 ${
-          !isOpen ? 'bg-transparent -z-50' : 'bg-black'
+          !isOpen ? 'bg-transparent !-z-50' : 'bg-black'
         }`}
       >
         <div
           className={`relative max-w-md h-auto shadow border-2 lg:border-4 border-white/50 bg-[#243d13] rounded-3xl  top-[50%] left-[50%] duration-200 translate-y-[-50%] translate-x-[-50%] ease-out scale-100 ${
-            !isOpen ? '!scale-0' : 'scale-100'
+            !isOpen ? '!scale-0' : '!scale-100'
           }`}
         >
           <div className="px-10 py-4 rounded-t">
