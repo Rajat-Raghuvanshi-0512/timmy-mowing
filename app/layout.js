2e5e8cec561fd6551360286cdbc9e6912cc26204
Rajat <rajat.karnal@gmail.com>
2023-10-02 20:37:22 +0530

    Updated  testimonials

--- app/layout.js
+++ app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
           src="https://www.googletagmanager.com/ns.html?id=GTM-MG2KHT7G"
           height="0"
           width="0"
-          style="display:none;visibility:hidden"
+          style={{ display: 'none', visibility: 'hidden' }}
         ></iframe>
       </noscript>
     </html>
