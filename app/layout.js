7b037c0a663ae42a46be9620f542f3ac187e45e3
Rajat <rajat.karnal@gmail.com>
2023-10-03 15:07:16 +0530

    Removed google tags

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import Script from 'next/script';
+// import Script from 'next/script';
 import './globals.css';
 import localFont from 'next/font/local';
 
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
       </head>
       <body className={myFont.className}>
         {children}
-        <noscript
+        {/* <noscript
           dangerouslySetInnerHTML={{
             __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MG2KHT7G"
 height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
@@ -30,7 +30,6 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
         <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=AW-11340606351"
-          defer
         ></Script>
         <Script
           strategy="afterInteractive"
@@ -38,8 +37,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
             __html: `(function(w,d,s,l,i){w[l]=w[l] ||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MG2KHT7G');`,
           }}
           id="x"
-          defer
-        ></Script>
+        ></Script> */}
       </body>
     </html>
   );
