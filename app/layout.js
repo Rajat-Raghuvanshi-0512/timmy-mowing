7f6f4ecb344804980d6c5daeee3bd8b22256ae9d
Rajat <rajat.karnal@gmail.com>
2023-10-05 15:22:59 +0530

    Added GTM

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import './globals.css';
 import localFont from 'next/font/local';
 
@@ -17,7 +18,28 @@ export default function RootLayout({ children }) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <body className={myFont.className}>{children}</body>
+      <body className={myFont.className}>
+        {children}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MG2KHT7G"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        ></noscript>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-11340606351"
+          defer
+        ></Script>
+        <Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l] ||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MG2KHT7G');`,
+          }}
+          id="x"
+          defer
+        ></Script>
+      </body>
     </html>
   );
 }
